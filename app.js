8f0a88274017c7942640905eddbc338fb57171a6
sayman <mdrabbi.asm@gmail.com>
2022-02-13 19:13:55 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+function generateNumber(){
+    const pinNumber=  Math.round(Math.random()*1000000);
+    const pin = pinNumber + '';
+    if(pin.length ===6){
+        return pin;
+    }
+    else{
+        return generateNumber();    
+    }
+}
+const pinInput = document.getElementById('pin-input');
+document.getElementById('generate-pin').addEventListener('click',function(){
+      pinInput.value = generateNumber();
+})
+const buttons = document.getElementsByClassName('button');
+const calcInput =document.getElementById('calc-input');
+for (const button of buttons){
+    button.addEventListener('click',function(e){
+       
+       switch (e.target.innerText){
+           case ('C'):
+               calcInput.value = ''
+               break;
+               case ('←'):
+                   calcInput.value = calcInput.value.slice(0,-1);
+                   break;
+           default:
+               calcInput.value +=e.target.innerText;
+       }
+   })
+}
+const validation = document.getElementById('validation');
+document.getElementById('submit').addEventListener('click',function(){
+      const notifySuccess =document.getElementById('notify-success');
+      const notifyWrong=document.getElementById('notify-wrong');
+      if(validation.innerText == 0){
+         alert ("Please Try Again Letter");
+      }
+      else if(parseInt( validation.innerText)<= 6){
+        if(pinInput.value == calcInput.value){
+            notifySuccess.style.display ='block';
+            notifyWrong.style.display = 'none'
+            validation.innerText = 6;
+        }
+        else{
+           notifySuccess.style.display ='none';
+           notifyWrong.style.display = 'block'
+           validation.innerText = parseInt(validation.innerText) - 1;
+        }
+      }
+})
